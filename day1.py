3759095cf4be9a557cd276775f731a5a5d4d2a47
rosemaria96 <ranirosejose96@gmail.com>
2025-12-02 18:09:07 +0530

    Data Loading, Inspection, Cleaning

--- day1.py
+++ day1.py
@@ -1,3 +1,5 @@
+
+# Module 1: Data Loading & Inspection
 import pandas as pd
 df = pd.read_csv(r"C:\Users\ACER\Documents\Excel\netflix_data.csv")
 
@@ -23,4 +25,28 @@ print(df.select_dtypes(include=['number']).columns)
 print("\nCategorical columns:")
 print(df.select_dtypes(include=['object']).columns)
 
+# module 2: cleaning the datasets
+df.fillna({'director': 'Unknown','cast': 'Unknown','rating': 'Unknown','country': 'Unknown','date_added': 'Unknown','duration': 'Unknown'}, inplace=True)
+print(df.isnull().sum()) #verification
+
+df['date_added'] = pd.to_datetime(df['date_added'], errors='coerce')
+df['date_added_raw'] = df['date_added'].dt.strftime('%Y-%m-%d')
+df['date_added_raw'] = df['date_added_raw'].fillna("Unknown")  # date to string
+
+
+print(df['date_added'].dtype)
+print(df['date_added'].head())
+
+print(df['duration'].unique()[:20]) #to print duration of first 20 
+print(df.groupby('type')['duration'].unique()) # durations per time
+print(df['duration'].str.contains('min', na=False).sum())  # chack patterns
+print(df['duration'].str.contains('Season',na=False).sum()) # check patterns
+
+text_cols = ['title', 'director', 'cast', 'country', 'listed_in', 'description']
+df[text_cols] = df[text_cols].apply(lambda x: x.str.lower())
+print(df['title'].head())  # convert text columns to lowercase
+
+df.drop_duplicates(inplace =True) 
+df.reset_index(drop=True, inplace=True) #remove duplicate rows
+print(df.shape) #verification
 
